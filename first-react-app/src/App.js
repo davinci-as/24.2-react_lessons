dc983ecb3324cb94930c9b65efe40219651628a2
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-10-19 11:45:29 -0300

    refactor: extract pets array

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,25 +1,21 @@
 import './App.css';
+import {
+  useState
+} from 'react';
 import PetsViewer from './components/PetsViewer';
 
+const URL = "http://localhost:3000/data/pets.json"
 function App() {
-  const pets = [
-    { 
-      nombre: "pato", 
-      imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlP43bXvIFTB5XmGqCJq9keyTQAmeZ0XZnTQ&s", 
-      habilitado: true },
-    { 
-      nombre: "gato", 
-      imagen: "https://static.nationalgeographicla.com/files/styles/image_3200/public/nationalgeographic_1468962.jpg?w=1900&h=1400", 
-      habilitado: true },
-    { 
-      nombre: "perro", 
-      imagen: "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/15665/production/_107435678_perro1.jpg.webp", 
-      habilitado: false },
-    { 
-      nombre: "puma", 
-      imagen: "https://media.istockphoto.com/id/682954094/es/foto/puma.jpg?s=612x612&w=0&k=20&c=QhAwTbu_sVPR4gnI2yLwpmjV6pcXI0zxiLI6cbAp7lo=", 
-      habilitado: true }
-  ];
+  const [pets, setPets] = useState();
+
+  /*fetch(URL)
+    .then(response => response.json())
+    .then(response => response.response)
+    .then(response => {
+      console.log("response", response);
+      return response;
+    })
+    .then(pets => setPets(pets))*/
 
   return (
     <div className="App">
