3f85b7dca19f8d3bafe6c490e8f156636f58ed94
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-10-19 12:20:30 -0300

    feat: add render list

--- first-react-app/src/components/PetsViewer/index.js
+++ first-react-app/src/components/PetsViewer/index.js
@@ -2,13 +2,21 @@ import React from "react";
 import PetCard from "../PetCard";
 import styles from "./styles.module.css";
 
-const PetsViewer = ({pets}) => <div className={styles.container}>
-    { pets
-        .filter(({habilitado}) => habilitado)
-        .map((mascota, index) => 
-            <PetCard key={index} {...mascota}/>
-        )
-    }
-</div>
-
+const PetsViewer = ({pets}) => {
+    const list = []
+    return <><div className={styles.container}>
+        { pets
+            .filter(({habilitado}) => habilitado)
+            .map((mascota, index) => 
+                <PetCard key={index} {...mascota}/>
+            )
+        }
+    </div>
+    <div>
+        <ul>
+            {list.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
+    </div>
+    </>
+}
 export default PetsViewer;
