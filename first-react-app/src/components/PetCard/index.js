13a34dd66c064c2d23b4c46785e2883beb335559
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-10-19 12:10:36 -0300

    feat: update card styles

--- first-react-app/src/components/PetCard/index.js
+++ first-react-app/src/components/PetCard/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from './styles.module.css';
 
-const PetCard = ({ nombre, imagen }) => <div className={styles.hide}>
+const PetCard = ({ nombre, imagen }) => <div className={styles.container}>
     <p>{nombre}</p>
     <img src={imagen} className={styles.image}/>
 </div>
--- first-react-app/src/components/PetsViewer/index.js
+++ first-react-app/src/components/PetsViewer/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import PetCard from "../PetCard";
+import styles from "./styles.module.css";
 
-const PetsViewer = ({pets}) => pets
-    .filter(({habilitado}) => habilitado)
-    .map((mascota, index) => <PetCard key={index} {...mascota}/>)
+const PetsViewer = ({pets}) => <div className={styles.container}>
+    { pets
+        .filter(({habilitado}) => habilitado)
+        .map((mascota, index) => 
+            <PetCard key={index} {...mascota}/>
+        )
+    }
+</div>
 
 export default PetsViewer;
