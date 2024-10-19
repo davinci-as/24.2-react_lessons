496786ec7d4e35240ea37fb02fce5fa36bdf1647
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-10-19 12:13:06 -0300

    feat: add card calltoaction

--- first-react-app/src/components/PetCard/index.js
+++ first-react-app/src/components/PetCard/index.js
@@ -4,6 +4,7 @@ import styles from './styles.module.css';
 const PetCard = ({ nombre, imagen }) => <div className={styles.container}>
     <p>{nombre}</p>
     <img src={imagen} className={styles.image}/>
+    <button className={styles.callToAction}>Agregar a carrito</button>
 </div>
 
 export default PetCard;
